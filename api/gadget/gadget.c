0552de057734599d73343bb9f9c488bdd4e16f34
-TOSH- <tosh@t0x0sh.org>
2015-03-29 16:12:08 +0200

    Adding filters to gadget searching

--- api/gadget/gadget.c
+++ api/gadget/gadget.c
@@ -4,8 +4,9 @@ r_gadget_s* r_gadget_new(void) {
   return r_utils_calloc(1, sizeof(r_gadget_s));
 }
 
-int r_gadget_handle_init(r_gadget_handle_s *g_handle, r_binfmt_arch_e arch, r_disa_flavor_e flavor, int filter) {
+int r_gadget_handle_init(r_gadget_handle_s *g_handle, r_binfmt_arch_e arch, r_disa_flavor_e flavor, int filter, int depth) {
   assert(g_handle != NULL);
+  assert(depth > 0);
 
   if(!r_disa_init(&g_handle->disa, arch))
     return 0;
@@ -14,6 +15,7 @@ int r_gadget_handle_init(r_gadget_handle_s *g_handle, r_binfmt_arch_e arch, r_di
     return 0;
   }
 
+  g_handle->depth = depth;
   g_handle->filter = filter;
   g_handle->g_hash = r_utils_hash_new(free);
 
@@ -28,48 +30,40 @@ void r_gadget_handle_close(r_gadget_handle_s *g_handle) {
   r_utils_hash_free(&g_handle->g_hash);
 }
 
-static addr_t r_gadget_find_gadget_end(r_gadget_handle_s *g_handle, addr_t addr, u8 *code, u32 code_size) {
-  addr_t offset;
-
-  offset = 0;
-
-  if(/* !r_disa_end_is_call(&g_handle->disa) && */
-	/* !r_disa_end_is_jmp(&g_handle->disa) && */
-	/* !r_disa_end_is_ret(&g_handle->disa) && */
-	offset < code_size) {
-    r_disa_code(&g_handle->disa, code+offset, code_size-offset, addr+offset, 1);
-    offset++;
-  }
-
-  if(g_handle->disa.instr_lst.count)
-    return offset;
-
-  return R_BINFMT_BAD_ADDR;
-}
-
 void r_gadget_update(r_gadget_handle_s *g_handle, addr_t addr, u8 *code, u32 code_size) {
-  addr_t offset, cur_offset;
   r_gadget_s *gadget;
   r_utils_hash_elem_s *h_elem;
+  u32 i;
+  int j;
 
   assert(g_handle != NULL);
   assert(g_handle->g_hash != NULL);
 
-  cur_offset = 0;
-  offset = 0;
+  for(i = 0; i < code_size; i++) {
+    for(j = 1; j <= g_handle->depth; j++) {
+      r_disa_code(&g_handle->disa, code+i, code_size-i, addr+i, j);
 
-  while(offset < code_size &&
-	(cur_offset = r_gadget_find_gadget_end(g_handle, addr+offset, code+offset, code_size-offset)) != R_BINFMT_BAD_ADDR) {
+      if(g_handle->disa.instr_lst.count > 0) {
+	if(r_disa_end_is_call(&g_handle->disa) ||
+	   r_disa_end_is_jmp(&g_handle->disa) ||
+	   r_disa_end_is_ret(&g_handle->disa)) {
 
-    gadget = r_gadget_new();
-    gadget->addr = addr+offset+cur_offset;
-    gadget->gadget = r_disa_instr_lst_to_str(&g_handle->disa);
+	  gadget = r_gadget_new();
+	  gadget->addr = addr+i;
+	  gadget->gadget = r_disa_instr_lst_to_str(&g_handle->disa);
 
-    h_elem = r_utils_hash_elem_new(gadget, (u8*)gadget->gadget, strlen(gadget->gadget));
-    if(!r_utils_hash_elem_exist(g_handle->g_hash, h_elem->key, h_elem->key_len))
-      r_utils_hash_insert(g_handle->g_hash, h_elem);
+	  h_elem = r_utils_hash_elem_new(gadget, (u8*)gadget->gadget, strlen(gadget->gadget));
 
-    offset += cur_offset + 1;
+	  if((!g_handle->filter || r_gadget_filter(gadget->gadget, g_handle->disa.arch, g_handle->disa.flavor)) &&
+	     !r_utils_hash_elem_exist(g_handle->g_hash, h_elem->key, h_elem->key_len)) {
+	    r_utils_hash_insert(g_handle->g_hash, h_elem);
+	  } else {
+	    free(gadget->gadget);
+	    free(gadget);
+	    free(h_elem);
+	  }
+	}
+      }
+    }
   }
-
 }

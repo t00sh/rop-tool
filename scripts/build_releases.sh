9b85e55eef9f4d4bf18b9cc12f4fc203d6791152
-TOSH- <tosh@t0x0sh.org>
2015-04-05 22:48:45 +0200

    Changes in build_release script

--- scripts/build_releases.sh
+++ scripts/build_releases.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 
-if test -z $1
+# How to release :
+# - Modify CHANGES file
+# - Modify README.md
+# - Commit on dev branch
+# - Run this script
+
+if test ! -z $1
 then
-    echo "Usage : $0 <version>"
-    exit 1
+    ONLY_BUILD=1
 fi
 
 rm *.asc
 
-OLD_VERSION=$(sed -rn "s/^VERSION = (.*)/\1/p" Makefile.inc)
-NEW_VERSION=$1
+if test ! -z $ONLY_BUILD
+then
+    OLD_VERSION=$(sed -rn "s/^VERSION = (.*)/\1/p" Makefile.inc)
+    NEW_VERSION=$1
+    
+    sed -i -r "s/VERSION = .+/VERSION = $NEW_VERSION/g" Makefile.inc
+    
+    git tag v$NEW_VERSION
+    git checkout master
+    git merge dev
+    git push --tags
+    git push
+fi
 
-sed -i -r "s/VERSION = .+/VERSION = $NEW_VERSION/g" Makefile.inc
-
-git tag v$NEW_VERSION
 
 # Linux
 ARCH=x86 make -f Makefile clean
@@ -30,4 +43,7 @@ ARCH=x86-64 make -f Makefile.windows clean
 ARCH=x86-64 make -f Makefile.windows release
 
 
-rsync -Pravdtze ssh rop-tool* www-data@t0x0sh.org:~/t0x0sh/rop-tool/releases/$NEW_VERSION/
+if test -z ! $ONLY_BUILD
+then
+    rsync -Pravdtze ssh rop-tool* www-data@t0x0sh.org:~/t0x0sh/rop-tool/releases/$NEW_VERSION/
+fi
